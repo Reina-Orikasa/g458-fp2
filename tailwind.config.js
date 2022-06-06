210de808d93e91fca74386f21315ef3b50171d02
Reina Orikasa <tammichael76@gmail.com>
2022-06-06 01:35:08 -0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        2020: {
+          0: '#ffffd4',
+          1: '#fee391',
+          2: '#fec44f',
+          3: '#fe9929',
+          4: '#ec7014',
+          5: '#cc4c02',
+          6: '#8c2d04',
+        },
+      }
+    },
+  },
+  plugins: [],
+}
